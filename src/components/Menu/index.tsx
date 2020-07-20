b50d02e2b00c0737ec7d047d6dabfc987e625f68
zhenyulei <zhenyulei@jd.com>
2020-07-20 10:24:11 +0800

    update

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,11 @@
+import MenuBox from './menuBox'
+import SubMenu from './subMenu'
+import MenuItem from './menuItem'
+import './style/index.scss'
+
+const Menu = {
+    MenuBox,
+    SubMenu,
+    MenuItem
+}
+export default Menu
